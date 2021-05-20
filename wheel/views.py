b3968dc8b2517d357e55dde361d2b2dd83556877
Daniel Roy <jamesroydaniel@gmail.com>
2021-05-20 18:18:06 +0200

    adds create event page

--- wheel/views.py
+++ wheel/views.py
@@ -1,9 +1,12 @@
 from flask import Blueprint, render_template
-from .forms import CreateEventForm
 
 
 main = Blueprint('main', __name__)
 events = Blueprint('events', __name__, url_prefix='/events')
+
+@events.route('/create')
+def create():
+    return render_template('create.html')
 
 @events.route('/')
 def viewAll():
