2235de32988a4c0abe6dd326d3600c9a2ae5aa75
n10534261 <n10534261@qut.edu.au>
2021-05-30 18:15:32 +1000

    initial commit

--- wheel/__init__.py
+++ wheel/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+#test heroku works
 
 
 db = SQLAlchemy()
